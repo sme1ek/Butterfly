69d89aa07f399c223bff4834f5b01c8cf4b46cea
sme1ek <sme1ek88@gmail.com>
2016-03-28 07:50:30 +0800

    Added username column, user profile pages, and username sign-up form. Also URL to redirect to USER page if the USERNAME exists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,15 @@ class PagesController < ApplicationController
   end
 
   def profile
+    # get the user username from the URL as :id
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      # redirect to user not found page
+      redirect_to root_path, :notice=> "User not found"
+    end
   end
-
+  
   def explore
   end
 end
